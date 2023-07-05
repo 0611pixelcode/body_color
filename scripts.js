df703604c87c68cf8be03b65a3e4640572c63552
0611pixelcode <jahongirpixelcode@gmail.com>
2023-07-05 18:47:59 +0300

    js btn collor funksiyasi

---
+++ scripts.js
@@ -0,0 +1,11 @@
+let elBtn = document.querySelector('button')
+
+elBtn.addEventListener('click', function(){
+  document.body.style.backgroundColor = 'red'
+})
+
+elBtn.addEventListener('dblclick', function(){
+  document.body.style.backgroundColor = 'blue'
+})
+
+
